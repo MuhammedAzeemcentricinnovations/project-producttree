541294f1a6964dead965af98ea76f2d9e317c91b
MuhammedAzeemcentricinnovations <azeemcentricinnovations@gmail.com>
2023-09-12 14:47:39 +0530

    codes are avilible for calculating sugar weight and price, pencount and price  and oilvolume and price

--- bin/product_treecycle.dart
+++ bin/product_treecycle.dart
@@ -1,5 +1,81 @@
-import 'package:product_treecycle/product_treecycle.dart' as product_treecycle;
 
-void main(List<String> arguments) {
-  print('Hello world: ${product_treecycle.calculate()}!');
+
+
+abstract class Product {
+  Product(this.price, {this.weight, this.volume,  this.count});
+
+  int price;
+  int? weight;
+  int? volume;
+  int? count;
+  late int totalPrice;
+
+  void calculate();
 }
+
+class Pen extends Product {
+  Pen({required price, required count}) : super(price,count: count);
+  void computePenname() {
+   ;
+  }
+
+  @override
+  void calculate() {
+
+    if (count!=null){
+     totalPrice = price * count!;
+      print("3 pen price is$totalPrice Rupeess");
+
+    }
+    
+     
+    
+  }
+}
+
+
+
+
+
+  class Oil extends Product{
+  Oil ({ required price,required volume}):super(price,volume: volume);
+  
+  @override
+  void calculate() {
+    if (volume!=null){
+     totalPrice=price * volume!;
+      print("10 liter oil price is $totalPrice Rs");
+    }
+  }
+}
+
+
+
+class Sugar extends Product{
+  Sugar({ required price,required weight}):super(price,weight: weight);
+
+
+  @override
+  void calculate() {
+
+    if (weight!=null){
+     totalPrice=price * weight!;
+      print(" 35 kg sugar price is $totalPrice Rs");
+    }
+        
+  }}
+
+
+void main() {
+  final prctprice= Pen(price: 12, count: 3);
+  final prctweight=Sugar(price: 35, weight: 23);
+  final prctvolume=Oil(price: 105, volume: 50);
+
+
+//   // prctprice.computePenname();
+
+  prctvolume.calculate();
+    prctprice.calculate();
+      prctweight.calculate();
+}
+
--- test/product_treecycle_test.dart
+++ test/product_treecycle_test.dart
@@ -1,8 +1,83 @@
 import 'package:product_treecycle/product_treecycle.dart';
 import 'package:test/test.dart';
 
+
+
+
+abstract class Product {
+  Product(this.price, {this.weight, this.volume,  this.count});
+
+  int price;
+  int? weight;
+  int? volume;
+  int? count;
+  late int totalPrice;
+
+  void calculate();
+}
+
+class Pen extends Product {
+  Pen({required price, required count}) : super(price,count: count);
+  void computePenname() {
+   ;
+  }
+
+  @override
+  void calculate() {
+
+    if (count!=null){
+     totalPrice = price * count!;
+      print("3 pen price is$totalPrice Rupeess");
+
+    }
+    
+     
+    
+  }
+}
+
+
+
+
+
+  class Oil extends Product{
+  Oil ({ required price,required volume}):super(price,volume: volume);
+  
+  @override
+  void calculate() {
+    if (volume!=null){
+     totalPrice=price * volume!;
+      print("10 liter oil price is $totalPrice Rs");
+    }
+  }
+}
+
+
+
+class Sugar extends Product{
+  Sugar({ required price,required weight}):super(price,weight: weight);
+
+
+  @override
+  void calculate() {
+
+    if (weight!=null){
+     totalPrice=price * weight!;
+      print(" 35 kg sugar price is $totalPrice Rs");
+    }
+        
+  }}
+
+
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  final prctprice= Pen(price: 12, count: 3);
+  final prctweight=Sugar(price: 35, weight: 23);
+  final prctvolume=Oil(price: 105, volume: 50);
+
+
+//   // prctprice.computePenname();
+
+  prctvolume.calculate();
+    prctprice.calculate();
+      prctweight.calculate();
 }
